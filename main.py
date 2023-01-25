e9002f48502f62a9fc1214890558630b611b4198
joaoluccas <joaoluccas66@gmail.com>
2023-01-25 11:56:49 -0300

    primera versão final

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import random
 
 def play():
-    user_wins = 0
-    computer_wins = 0
+    win = 0
+    lose = 0
     tie = 0
     options = ['r', 'p', 's']
-    while True :
-        user = input('Escolha [r]ock ,[p]aper, [s]cissors ou [e]xit: ')
+    while True:
+        user = input('---Escolha um das opções---\n [r]ock ,[p]aper, [s]cissors ou [e]xit: ')
         
         if user == 'e':
-            print(result())
+            print('\nResultado:')
+            print(f'Vitórias: {win}')
+            print(f'Empates: {tie}')
+            print(f'Derrotas: {lose}\n')
+            print(' Você está se desconectantando\n \
+Feito por : João Luccas Marques')
             break
         elif user not in options :
             print('Escolha r, p, s .')
@@ -20,31 +25,27 @@ def play():
         computer = random.choice(options)
 
         if computer == user :
+            tie += 1
             print('Empate')
             print(f'O computador escolheu o mesmo que você "{computer}".')
-            tie += 1
+            
             
 
         elif is_win(user, computer):
+            win += 1
             print('Você ganhou!')
             print(f'O computador escolheu "{computer}".')
-            user_wins += 1
+          
         else:
+            lose += 1
             print('Você perdeu!')
             print(f'O computador escolheu "{computer}".')
 
-            computer_wins += 1
         continue
 def is_win(player, opponent):
     if (player == 'r'and opponent =='s' or player == 'p'and opponent == 'r' \
         or player == 's' and opponent == 'p'):
         return True
-
-def result(ganhou, empate, perdeu):
-    print('\nResultado:')
-    print(f'Vitórias: {ganhou}')
-    print(f'Empates: {empate}')
-    print(f'Derrotas: {perdeu}\n')
 
 def main():
     play()
