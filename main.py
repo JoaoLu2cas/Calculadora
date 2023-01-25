9f78d83dd12dac216af3d49b9fb45bff816972b6
joaoluccas <joaoluccas66@gmail.com>
2023-01-25 11:27:56 -0300

    New updates

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 import random
-user_wins = 0
-bot_wins = 0
 
-escolhas = ['pedra', 'papel', 'tesoura']
-print('Seja bem-vindo ao Jokempô')
+def play():
+    user_wins = 0
+    computer_wins = 0
+    tie = 0
+    options = ['r', 'p', 's']
+    while True :
+        user = input('Escolha [r]ock ,[p]aper, [s]cissors ou [e]xit: ')
+        
+        if user == 'e':
+            print(result())
+            break
+        elif user not in options :
+            print('Escolha r, p, s .')
+            continue
+        print('JO...')
+        print('KEM...')
+        print('PO!!!')
+        computer = random.choice(options)
 
-def enter():
-    while True:
-        mode = input(('Escolha [r]pedra [p]papel ou [s]tesoura: ')).lower()
-        if mode in 'r rock p paper s scissors'.split():
-            return mode
-        else :
-            print('Escolha r,p ou s')
+        if computer == user :
+            print('Empate')
+            print(f'O computador escolheu o mesmo que você "{computer}".')
+            tie += 1
             
-def options():
-    random_choice = random.choices(escolhas)
-    if random_choice == 'pedra' and enter() == 's':
-        print('você ganhou')
-        user_wins += 1
-    elif random_choice == 'papel' and enter() == 'r':
-        print('você ganhou')
-        user_wins += 1
-    elif random_choice == 'tesoura' and enter() == 'p' :
-        print('você ganhou')
-        user_wins += 1
-    else:
-        print('Você perdeu')
-        bot_wins += 1
+
+        elif is_win(user, computer):
+            print('Você ganhou!')
+            print(f'O computador escolheu "{computer}".')
+            user_wins += 1
+        else:
+            print('Você perdeu!')
+            print(f'O computador escolheu "{computer}".')
+
+            computer_wins += 1
+        continue
+def is_win(player, opponent):
+    if (player == 'r'and opponent =='s' or player == 'p'and opponent == 'r' \
+        or player == 's' and opponent == 'p'):
+        return True
+
+def result(ganhou, empate, perdeu):
+    print('\nResultado:')
+    print(f'Vitórias: {ganhou}')
+    print(f'Empates: {empate}')
+    print(f'Derrotas: {perdeu}\n')
 
 def main():
-    enter()
-    options()
+    play()
+    
+
 main()
